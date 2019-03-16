02e1c38c279b3b50e0fd570eb9b88757ac5742e9
nattawutv <44128241+nattawutv@users.noreply.github.com>
2019-03-17 00:33:19 +0700

    fix serializer

--- buyer/serializers.py
+++ buyer/serializers.py
@@ -2,6 +2,26 @@ from rest_framework import serializers
 
 from portal.models import Item
 from portal.models import Price
+from portal.models import Category
+
+
+class ItemSerializer(serializers.ModelSerializer):
+
+    div_name = serializers.SerializerMethodField()
+
+    store_name = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_store_name(obj):
+        return obj.cat.div.store.store_name
+
+    @staticmethod
+    def get_div_name(obj):
+        return obj.cat.div.div_name
+
+    class Meta:
+        model = Item
+        fields = ('code', 'itm_name', 'img_path', 'rating', 'delivery_fee', 'is_recommend', 'sale_total')
 
 
 class ItemCatDivStoreSerializer(serializers.ModelSerializer):
@@ -21,6 +41,8 @@ class ItemCatDivStoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ('code', 'itm_name', 'img_path', 'rating', 'delivery_fee', 'is_recommend', 'sale_total')
+
+
 
 
 class PricingItemSerializer(serializers.ModelSerializer):
